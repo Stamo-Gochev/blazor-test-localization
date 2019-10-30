164e4b4a32b2dd428993ee68f26b721da469490e
Stamo-Gochev <stamo_gochev@yahoo.com>
2019-10-30 16:24:50 +0200

    feat: add sample

---
+++ blazor-test-localization/Components/LocalizedComponent.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_test_localization.Components
+{
+    public partial class LocalizedComponent : ComponentBase
+    {
+        [Inject]
+        public IStringLocalizer<LocalizedComponent> Localizer { get; set; }
+        //public IStringLocalizer Localizer { get; set; }
+
+        public string Title { get; set; }
+
+        protected override void OnInitialized()
+        {
+
+            base.OnInitialized();
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+
+            }
+
+            Title = Localizer["key1"];
+            base.OnAfterRender(firstRender);
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected override bool ShouldRender()
+        {
+            return base.ShouldRender();
+        }
+
+    }
+}
