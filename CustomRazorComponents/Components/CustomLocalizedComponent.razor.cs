875940700e8549d29e773651b9eb762d0ff1071d
Stamo-Gochev <stamo_gochev@yahoo.com>
2019-11-06 14:23:03 +0200

    feat: localize external library

--- CustomRazorComponents/Components/CustomLocalizedComponent.razor.cs
+++ CustomRazorComponents/Components/CustomLocalizedComponent.razor.cs
@@ -1,19 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace blazor_test_localization.Components
+namespace CustomRazorComponents.Components
 {
-    public partial class LocalizedComponent : ComponentBase
+    public partial class CustomLocalizedComponent : ComponentBase
     {
-        [Inject]
-        public IStringLocalizer<LocalizedComponent> Localizer { get; set; }
+        //[Inject]
+        //public IStringLocalizer<CustomLocalizedComponent> Localizer { get; set; }
         //public IStringLocalizer Localizer { get; set; }
 
         public string Title { get; set; }
