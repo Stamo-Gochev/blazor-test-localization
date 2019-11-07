50851001e8e8763feaff4561a33bbfdd7cc16d77
Stamo-Gochev <stamo_gochev@yahoo.com>
2019-11-07 15:17:18 +0200

    chore: add wasm

---
+++ wasm_localization/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace wasm_localization.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
